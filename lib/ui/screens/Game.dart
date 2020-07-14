0658133a2608dbc6d84dc1d6eccc24e3ad147226
danielgrgly <danielgrgly@gmail.com>
2020-07-14 12:06:06 +0200

    initialise routing

---
+++ lib/ui/screens/Game.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("game"),
+      ),
+    );
+  }
+}
