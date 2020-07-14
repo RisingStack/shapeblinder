527fa20584b322275626a4ea593025fc3b0ec0e1
danielgrgly <danielgrgly@gmail.com>
2020-07-14 12:33:50 +0200

    home initial design

--- lib/ui/screens/Home.dart
+++ lib/ui/screens/Home.dart
@@ -4,13 +4,31 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "shapeblinder",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24.0,
-          ),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "shapeblinder",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 34.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "a game with the lights off",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+                color: Colors.grey[600],
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
         ),
       ),
     );
